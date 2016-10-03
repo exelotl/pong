ef7366b456abc1cc52a767a9df1a9059fa39d806
octaj0n <jonathankeslake@gmail.com>
2016-10-03 14:05:47 +0100

    Revert "Jon's working on shit"

--- entities/paddles.lua
+++ entities/paddles.lua
@@ -10,9 +10,9 @@ end
 
 function Paddle:update(dt)
 end
-
 function Paddle:draw()
 end
+
 
 local BobLong = oo.class(Paddle)
 
@@ -54,61 +54,7 @@ function Sophia:draw()
 end
 
 
-local DrStoptagon = oo.class(Paddle)
-local size = 40
-local sideln = 0.4142 * (size*2)
-local sidetria = sideln/1.41421
-
-function DrStoptagon:init(scene, input, x, y)
-	Paddle.init(self, scene, input, x, y)
-	self.body = lp.newBody(scene.world, x, y, "dynamic")
-	self.body:setFixedRotation(true)
-	self.shape = lp.newPolygonShape(-size + sidetria, -size, -- top left
-                                   size - sidetria, -size, -- top right
-                                   size,- size + sidetria, -- right top
-                                   size, size - sidetria, -- right bottom
-                                   size - sidetria, size, -- bottom right
-                                  -size + sidetria, size, -- bottom left
-                                  -size, - size + sidetria, -- left bottom
-                                  -size, size - sidetria ) -- left top
-
-	self.fixture = lp.newFixture(self.body, self.shape)
-end
-
-function DrStoptagon:update(dt)
-	local i = self.input
-	local vx = (i:get("right") - i:get("left")) * 400 
-	local vy = (i:get("down") - i:get("up")) * 400 
-	local w = (i:get("rotr") - i:get("rotl")) * 5 
-	self.body:setLinearVelocity(vx, vy)
-	self.body:setAngularVelocity(w)
-end
-
-local SeriousSum = oo.class(Paddle)
-
-function SeriousSum:init(scene, input, x, y)
-	Paddle.init(self, scene, input, x, y)
-	self.body = lp.newBody(scene.world, x, y, "dynamic")
-	self.body:setFixedRotation(true)
-	self.shape = lp.newRectangleShape(40, 100)
-  self.shape2 = lp.newRectangleShape(100, 40)
-	self.fixture = lp.newFixture(self.body, self.shape)
-  self.fixture2 = lp.newFixture(self.body, self.shape2)
-  
-end
-
-function SeriousSum:update(dt)
-	local i = self.input
-	local vx = (i:get("right") - i:get("left")) * 400 
-	local vy = (i:get("down") - i:get("up")) * 400 
-	local w = (i:get("rotr") - i:get("rotl")) * 5 
-	self.body:setLinearVelocity(vx, vy)
-	self.body:setAngularVelocity(w)
-end
-
 return {
 	BobLong = BobLong,
 	Sophia = Sophia,
-  DrStoptagon = DrStoptagon,
-  SeriousSum = SeriousSum
 }
